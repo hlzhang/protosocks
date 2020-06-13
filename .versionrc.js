82dccdf682c880617f129868bc323b321cd8de47
hlzhang <hlzhang@users.noreply.github.com>
2020-06-13 20:31:33 +1200

    chore: bump version up.

--- .versionrc.js
+++ .versionrc.js
@@ -11,7 +11,12 @@
 // npx standard-version --dry-run
 module.exports = () => {
     return {
-        "bumpFiles": [],
+        "bumpFiles": [
+            {
+                "filename": "ver",
+                "type": "plain-text"
+            }
+        ],
         "packageFiles": [
             {
                 "filename": "ver",
